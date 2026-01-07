7b2309b3750a743f175d6e50ec5e7fb767c5948e
Shiburaj Pappu <69638955+shiburaj@users.noreply.github.com>
2026-01-07 10:09:19 +0530

    Add comments for personalized greeting implementation
    Added comments for a personalized greeting feature.

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-def add(a, b):
-    return a + b
+# AIM: Create a personalized greeting message
+#      User enters name: eg: Shibu. and the program should print "Hello, Shibu" 
+# Coder: 
+# Date:
+
+# Write your code here
